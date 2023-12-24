a9053f94c995b224ef0366c26ad7710303a24610
WellingtonDefassio <well.defassio@gmail.com>
2023-12-24 17:43:55 -0300

    setup start project folders and navigation and stack

---
+++ src/screens/ManageExpenses.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function ManageExpenses() {
+  return (
+    <View>
+      <Text> ManageExpenses </Text>
+    </View>
+  );
+}
---
+++ src/screens/RecentExpense.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function RecentExpense() {
+  return (
+    <View>
+      <Text> RecentExpense </Text>
+    </View>
+  );
+}
